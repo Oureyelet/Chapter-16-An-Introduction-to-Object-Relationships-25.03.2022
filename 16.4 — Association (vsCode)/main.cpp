478157cd96622711bfccbbc1739dbebdb56444b4
Mateusz <oureyelet@gmail.com>
2022-03-31 19:47:35 +0100

    16.4 — Association (vsCode)

--- 16.4 — Association (vsCode)/main.cpp
+++ 16.4 — Association (vsCode)/main.cpp
@@ -23,7 +23,7 @@ public:
     // We'll implement this function below Patient since we need Patient to be defined at that point
     friend std::ostream& operator<< (std::ostream& out, const Doctor& doctor);
 
-    const std::string& getName() { return m_name; }
+    const std::string& getName() const { return m_name; }
 
 };
 
@@ -48,7 +48,7 @@ public:
     // We'll implement this function below Doctor since we need Doctor to be defined at that point
     friend std::ostream& operator<< (std::ostream& out, const Patient& patient);
 
-    const std::string& getName() { return m_name; }
+    const std::string& getName() const { return m_name; }
 
     // We'll friend Doctor::addPatient() so it can access the private function Patient::addDoctor()
     friend void Doctor::addPatient(Patient& patient);
@@ -72,6 +72,7 @@ std::ostream& operator<< (std::ostream& out, const Doctor& doctor)
         return out;
     }
 
+    out << doctor.m_name << "  is seeing patients: ";
     for(const auto& name : doctor.m_patient)
     {
         out << name.get().getName() << ' ';
@@ -85,13 +86,76 @@ std::ostream& operator<< (std::ostream& out, const Patient& patient)
     if( patient.m_doctor.empty() )
     {
         out << patient.m_name << " has no doctors right now";
+
+        return out;
     }
 
-    for(const auto& name : patient.m_doctor)
-    {
-        out << name.get()
-    }
+    out << patient.m_name << " is seeing doctors: ";
+	for (const auto& name : patient.m_doctor)
+		out << name.get().getName() << ' ';
+
+    return out;
 }
+
+class Car
+{
+private:
+    std::string m_name{};
+    int m_ID;
+
+public:
+    Car(const std::string& name, int id)
+        :m_name{ name }, m_ID{ id }
+    {
+    }
+
+    const std::string& getName() const { return m_name; }
+    int getID() const { return m_ID; }
+
+};
+
+// Our CarLot is essentially just a static array of Cars and a lookup function to retrieve them.
+// Because it's static, we don't need to allocate an object of type CarLot to use it
+class CarLot
+{
+private:
+    static Car s_carLot[4];
+
+public:
+    CarLot() = delete;// Ensure we don't try to create a CarLot
+
+    static Car* getCar(int id)
+    {
+        for(int count{ 0 }; count < 4; ++count)
+        {
+            if(s_carLot[count].getID() == id)
+            {
+                return &(s_carLot[count]);
+            }
+        }
+        return nullptr;
+    }
+
+};
+
+Car CarLot::s_carLot[4]{ {"Prius", 4}, {"Corolla", 17}, {"Accord", 84}, {"Matrix", 62} };
+
+class Driver
+{
+private:
+    std::string m_name{};
+    int m_carId{};
+
+public:
+    Driver(const std::string& name, int id)
+        :m_name{ name }, m_carId{ id }
+    {
+    }
+
+    const std::string& getName() const { return m_name; }
+    int getID() const { return m_carId; }
+
+};
 
 int main()
 {
@@ -99,6 +163,39 @@ int main()
 
     //Implementing associations:
 
+    // Create a Patient outside the scope of the Doctor
+    Patient dave{ "Dave" };
+    Patient frank{ "Frank" };
+    Patient betsy{ "Betsy" };
+
+    Doctor james{ "James" };
+    Doctor scott{ "Scott" };
+
+    james.addPatient(dave);
+
+    scott.addPatient(dave);
+    scott.addPatient(betsy);
+
+    std::cout << james << '\n';
+    std::cout << scott << '\n';
+    std::cout << dave << '\n';
+    std::cout << frank << '\n';
+    std::cout << betsy << '\n';
+
+    //Reflexive association:
+
+    //Associations can be indirect:
+    Driver d{ "Franz", 17 };// Franz is driving the car with ID 17
+
+    Car* car{ CarLot::getCar(d.getID()) };// Get that car from the car lot
+
+    if(car)
+        std::cout << d.getName() << " is driving a " << car->getName() << '\n';
+    else
+        std::cout << d.getName() << " couldn't find his car\n";
+
+    //Composition vs aggregation vs association summary:
+    
 
     return 0;
 }
